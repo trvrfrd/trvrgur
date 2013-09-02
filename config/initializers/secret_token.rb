4ab8bae77abd1b05f5d284733eb52ac72e0b8e67
Trevor Ford <trevor.a.ford@gmail.com>
2013-09-02 11:26:40 -0700

    initial commit WOW

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trvrgur::Application.config.secret_token = '043904ab0d264156eaec79d372fad5c9e09e5486d484dda7d6c37ee11ca5dcb81d61413c2f593655030b84d796780af988e3ddaa5056108b4d9d2a615f614193'
