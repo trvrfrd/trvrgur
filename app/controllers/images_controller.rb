17d0fbd7a2f7d92397dbba3edf898dc4bb6490d0
Trevor Ford <trevor.a.ford@gmail.com>
2013-09-03 15:35:09 -0700

    image uploading works! implement basic images#new, #create, #show, #index

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,29 @@
+class ImagesController < ApplicationController
+
+  def create
+    @image = Image.new(params[:image])
+    @image.user_id = current_user ? current_user.id : nil
+    if @image.save
+      redirect_to image_url(@image)
+    else
+      render :new
+    end
+    
+  end
+
+  def index
+    @images = Image.all
+    render :index
+  end
+
+  def new
+    @image = Image.new
+    render :new    
+  end
+
+  def show
+    @image = Image.find(params[:id])
+    render :show
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Trvrgur::Application.routes.draw do
   resource :session, :only => [:create, :destroy, :new]
   resources :users, :except => [:index]
   resources :images
+
+  root :to => "images#index"
 end
