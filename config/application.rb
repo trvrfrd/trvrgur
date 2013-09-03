fed558eff40b0f0a1b856b995f8965b68c21d184
Trevor Ford <trevor.a.ford@gmail.com>
2013-09-03 10:08:12 -0700

    update config/application.rb => disable initialize_on_precompile

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,8 @@ module Trvrgur
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # DON'T do the thing tha breaks Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
