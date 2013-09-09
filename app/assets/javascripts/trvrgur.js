1979dc7b11325439f8059e6f80f93ccd6a48d87a
Trevor Ford <trevor.a.ford@gmail.com>
2013-09-09 13:59:28 -0700

    add basic root#root and bootstrap initial data

--- app/assets/javascripts/trvrgur.js
+++ app/assets/javascripts/trvrgur.js
@@ -3,11 +3,11 @@ window.Trvrgur = {
   Collections: {},
   Views: {},
   Routers: {},
-  initialize: function() {
-    alert('Hello from Backbone!');
+  initialize: function($el, user, albums, comments) {
+    Trvrgur.$el = $el;
+    Trvrgur.current_user = user;
+    Trvrgur.albums = albums;
+    Trvrgur.comments = comments;
+    console.log(Trvrgur);
   }
 };
-
-$(document).ready(function(){
-  Trvrgur.initialize();
-});
--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,5 +1,7 @@
 class RootController < ApplicationController
   def root
+    @albums = nil
+    @comments = nil
     render :root
   end
 end
