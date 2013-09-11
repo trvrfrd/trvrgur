8a29c770dd3a947952cfbb65aa48824a9af98c21
Trevor Ford <trevor.a.ford@gmail.com>
2013-09-11 09:04:02 -0700

    update AlbumShow and AlbumsIndex - top comments needs fixin'

--- app/assets/javascripts/views/albums/album_show.js
+++ app/assets/javascripts/views/albums/album_show.js
@@ -2,9 +2,19 @@ Trvrgur.Views.AlbumShow = Backbone.View.extend({
 
   template: JST['albums/show'],
 
+  events: {
+    "click .upvote"  : "upvote",
+    "click .downvote": "downvote",
+    "click .favorite": "favorite"
+  },
+
   render: function () {
     this.$el.html(this.template({ album: this.model }));
     return this;
-  }
+  },
+
+  downvote: function () {},
+  favorite: function () {},
+  upvote: function () {}
 
 });
